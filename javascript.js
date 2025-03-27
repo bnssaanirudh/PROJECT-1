542b57842a932df9b2c36d70a0a8d07efe71971a
Badampudi Agasthya Anirudh <anirudhbadampudi@gmail.com>
2025-03-27 15:56:42 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,5 +0,0 @@
-// Add smooth scroll for CTA button click
-document.querySelector('.cta-btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#signup').scrollIntoView({ behavior: 'smooth' });
-});
